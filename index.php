4d38d3098d71d3ff1f0dded6667acbfa01c8f5f1
Ahmed HACHICHA <ahmed.hachicha@sfax.r-iset.tn>
2020-12-04 20:04:17 +0100

    first

--- index.php
+++ index.php
@@ -1,53 +1,46 @@
 <?php
-    require "config.php";
+require "config.php";
 $data=constant("INPUT_DATA");
-    $tab = unserialize($data);
-    $cles=array_keys($tab);
+$tab = unserialize($data);
+$cles=array_keys($tab);
 ?>
-<style>
-    th, td {
-        padding: 15px;
-        text-align: left;
-    }
-    table, td, th {
-        border: 1px solid black;
-        text-align: left;
-    }
-    table {
-        border-collapse: collapse;
-        width: 100%;
-    }
-    th{
-        font-family: Segoe UI;
-    }
-    td{
-        font-family: Segoe UI;
-        font-size: larger;
-    }
-</style>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
 <h1 style="padding-left:20px;font-family: Segoe UI;font-size: 30px;font-weight: 600;">Our super table</h1>
-<table style=" width:50%;height: 150px;" >
+<table class="center">
     <thead style=" background-color:#00b0f0;height: 100%">
-        <tr>
-            <th>Item1</th>
-            <th>Item2</th>
-            <th>Item1 + Item2</th>
-            <th>Paire/impaire</th>
-        </tr>
+    <tr>
+        <th>Item1</th>
+        <th>Item2</th>
+        <th>Item1 + Item2</th>
+        <th>Paire/impaire</th>
+    </tr>
     </thead>
     <tbody>
-<?php
-for($i=0;$i< count($cles);$i++)
+    <?php
+    for($i=0;$i< count($cles);$i++)
     {
-$p='';
-$somme=$tab[$cles[$i]]+$cles[$i];
-if($somme % 2 == 1) 
-    $p='Impaire';
-else $p='Paire';
+        $p='';
+        $somme=$tab[$cles[$i]]+$cles[$i];
+        if($somme % 2 == 1)
+            $p='Impaire';
+        else $p='Paire';
 
-    echo '<tr><td>'.$cles[$i].'</td><td>'.$tab[$cles[$i]].'</td><td>'.$somme.'</td><td>'.$p.'</td></tr>';
-}
+        echo '<tr><td>'.$cles[$i].'</td><td>'.$tab[$cles[$i]].'</td><td>'.$somme.'</td><td>'.$p.'</td></tr>';
+    }
 
-?>
-</tbody>
+    ?>
+    </tbody>
 </table>
+
+</body>
+</html>
