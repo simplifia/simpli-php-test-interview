30e07888aa5e9afae8549acc16aebaeeac9ba03a
mayssoun1 <mayssa.bensalah3@gmail.com>
2020-12-17 13:35:03 +0100

    Create necessary files

---
+++ view.php
@@ -0,0 +1,50 @@
+<?php require("controller.php")?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+    <link rel="stylesheet" href="/test.css">
+    <style>
+     
+    </style>
+    <title>Formulaire en ligne</title>
+</head>  
+<body>
+<h2>Our super table</h2>
+
+<table style="width:50% ;position: relative">
+    <tr>
+      <th>item1</th>
+      <th>item2</th> 
+      <th>item1+item2</th>
+      <th>Paire/Impaire</th>
+    </tr>
+    <tr>
+      <td>
+      <?php 
+      
+      
+      ?>
+      </td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td></td>
+      <td></td>
+      <td></td>
+    </tr>
+  </table>
+  
+
+</body>
+</html>
