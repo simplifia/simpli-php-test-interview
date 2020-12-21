691046e6072cd64a3c57d30acc1f51b73bb8f28b
ag7 <alexis26999@gmail.com>
2020-12-21 15:06:05 +0100

    add style

--- my-work/views/layout.php
+++ my-work/views/layout.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>GUAY Alexis</title>
     <link rel="stylesheet" href="<?php echo WEB_ROOT ?>/assets/css/style.css">
 </head>
 
