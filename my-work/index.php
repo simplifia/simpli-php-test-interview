f1433fe8fc2bb6cba1731108277d96f172c2ac32
ag7 <alexis26999@gmail.com>
2020-12-21 14:13:17 +0100

    extract controller and action name

--- my-work/index.php
+++ my-work/index.php
@@ -8,4 +8,11 @@ $webRoot = str_replace('index.php', '', $_SERVER['SCRIPT_NAME']);
 $request = str_replace($webRoot, '', $_SERVER['REDIRECT_URL']);
 $request = explode('/', $request);
 
-d($request);
+$controller = $request[0] !== '' ? $request[0] : 'rawdata';
+$action = isset($request[1]) && $request[1] !== '' ? $request[1] : 'table';
+
+define('WEB_ROOT', $webRoot);
+
+p($controller);
+p($action);
+d(WEB_ROOT);
