0519cbc4cf96165ef92dbedc8e2dee8f1d507150
ag7 <alexis26999@gmail.com>
2020-12-21 14:22:27 +0100

    add autoload for model

--- my-work/core/core.php
+++ my-work/core/core.php
@@ -7,10 +7,19 @@ spl_autoload_register(function ($class) {
         $controller = substr($class, 0, -10);
 
         if (file_exists('controllers/' . $controller . '.php')) {
-            d('controller found');
+            require 'controllers/' . $controller . '.php';
+        } else {
+            echo 'Error 404 - Controller not found';
         }
     } elseif (preg_match('/Model$/', $class)) {
-        d($class);
+        // delete 'Model' of class name
+        $model = substr($class, 0, -5);
+        // if model file exist require else error
+        if (file_exists('models/' . $model . '.php')) {
+            require 'models/' . $model . '.php';
+        } else {
+            echo 'Error 404 - Model not found';
+        }
     }
 });
 
