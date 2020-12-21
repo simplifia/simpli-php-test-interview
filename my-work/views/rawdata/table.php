2ac505c030b444773de4b13ce16dbf038d2ee70d
ag7 <alexis26999@gmail.com>
2020-12-21 15:03:59 +0100

    create html table with rawsdata

--- my-work/views/rawdata/table.php
+++ my-work/views/rawdata/table.php
@@ -1,6 +1,30 @@
-<h1>Our Super Table</h1>
-
-
 <?php
 
-p($rawsData);
+/** @var RawdataModel[] $rawsData */
+
+?>
+<h1>Our Super Table</h1>
+
+<table>
+    <thead>
+        <tr>
+            <td>Item1</td>
+            <td>Item2</td>
+            <td>Item1 + Item2</td>
+            <td>Paire/Impaire</td>
+        </tr>
+    </thead>
+    <tbody>
+        <?php
+        foreach ($rawsData as $raw) {
+            echo '<tr>';
+            echo '<td>' . $raw->i1 . '</td>';
+            echo '<td>' . $raw->i2 . '</td>';
+            echo '<td>' . $raw->sum . '</td>';
+            echo '<td>' . $raw->desc . '</td>';
+            echo '</tr>';
+        }
+        ?>
+
+    </tbody>
+</table>
