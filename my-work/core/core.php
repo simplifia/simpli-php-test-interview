e5bd29cdc273f65b3ebef9d9fbe6b1c8b24b3321
ag7 <alexis26999@gmail.com>
2020-12-21 15:06:38 +0100

    fin du test

--- my-work/core/core.php
+++ my-work/core/core.php
@@ -22,17 +22,3 @@ spl_autoload_register(function ($class) {
         }
     }
 });
-
-// FUNCTION FOR DEBUG
-function p($data = null)
-{
-    echo '<pre>';
-    var_dump($data);
-    echo '</pre>';
-}
-
-function d($data = null)
-{
-    p($data);
-    die;
-}
